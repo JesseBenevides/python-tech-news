08536485db84cf3d71304565ce7d56845a98feea
Jesse Benevides <jessebenevides@gmail.com>
2022-07-25 11:27:59 -0300

    feat: add req 2 - implements scrape_novidades function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -15,7 +16,9 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    links = selector.css("a.cs-overlay-link::attr(href)").getall()
+    return links
 
 
 # Requisito 3
