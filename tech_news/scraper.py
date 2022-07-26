62762ad9a89250be264ffd02296bba7f7f3df4f8
Jesse Benevides <jessebenevides@gmail.com>
2022-07-26 13:27:26 -0300

    feat: add req 4     - implements scrape_noticia function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -30,7 +30,38 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("span.author a::text").get()
+    comments_count = selector.css(
+        "div.post-comments h5.title-block::text"
+    ).get()
+    summary = "".join(
+        selector.css("div.entry-content > p:nth-of-type(1) ::text").getall()
+    ).strip()
+    tags = selector.css("section.post-tags a::text").getall()
+    category = selector.css("span.label::text").get()
+
+    if comments_count is None:
+        comments_count = 0
+    else:
+        comments_count = int(
+            comments_count.replace("\n", "").replace("\t", "").split(" ")[0]
+        )
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
 
 
 # Requisito 5
